3a8ac392b959ac8274606109797e3987f65f5bc9
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-27 14:55:17 +0200

    added first route and index

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, './index.html');
+    res.sendFile(filePath);
+});
+
+//Avvio del server
+app.listen(3000, () => {
+    console.log('Server attivo sulla porta http://localhost:3000.');
+});
