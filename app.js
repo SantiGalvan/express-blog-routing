08e980f5e6de77cf7188e249c007d9c5cb655677
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-27 15:52:47 +0200

    added router and middleware router

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const postsRouter = require("./routers/posts.js");
 
 app.use(express.static('./public'));
 
+// Routes
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, './index.html');
     res.sendFile(filePath);
 });
+
+app.use('/posts', postsRouter);
 
 //Avvio del server
 app.listen(3000, () => {
